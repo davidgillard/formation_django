7fad552a7aef5eb2ff953b5e66b3c9229f2196ee
David GILLARD <david.gillard@ac-lille.fr>
2020-06-02 20:06:54 +0200

    ajout de la vue detail

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views 
 
 urlpatterns = [
-#    url(r'^$', views.index), # "/store" va appeller la methode index dans views.py
+    url(r'^$', views.listing), # "/store" va appeller la methode index dans views.py
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail),
 ]
+)
--- store/views.py
+++ store/views.py
@@ -7,7 +7,20 @@
 
 #from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ALBUMS
 
 def index(request):
         message = "Salut tout le monde"
         return HttpResponse(message)
+
+def listing(request):
+    albums = ["<li>{}</li>".format(album['name']) for album in ALBUMS]
+    message = """<ul>{}</ul>""".format("\n".join(albums))
+    return HttpResponse(message)
+
+def detail(request, album_id):
+    id = int(album_id) # make sure we have an integer.
+    album = ALBUMS[id] # get the album with its id.
+    artists = " ".join([artist['name'] for artist in album['artists']]) # grab artists name and create a string out of it.
+    message = "Le nom de l'album est {}. Il a été écrit par {}".format(album['name'], artists)
+    return HttpResponse(message)
